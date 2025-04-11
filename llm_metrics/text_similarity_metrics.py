5b4c7c05cdee1867da14c14a763a6dde0de9d72e
Pallav Koppisetti <koppisettipallav@gmail.com>
2025-04-11 14:58:09 -0400

    Added SequenceMatcherSimilarity class

--- llm_metrics/text_similarity_metrics.py
+++ llm_metrics/text_similarity_metrics.py
@@ -1,3 +1,4 @@
+from difflib import SequenceMatcher
 from typing import Any, Dict, Iterable, List, Optional, Union
 
 import numpy as np
@@ -243,3 +244,79 @@ class LevenshteinDistance(BaseMetric):
                 self.calculate(gen, ref, calculate_ratio, params)
                 for gen, ref in zip(gen_texts, ref_texts)
             ]
+
+
+class SequenceMatcherSimilarity(BaseMetric):
+    """
+    This class calculates similarity ratio between texts using the ratio() method from difflib.SequenceMatcher,
+    which returns a float in the range [0, 1] indicating how similar the sequences are.
+
+    Supports calculation for individual sentence pairs and for batches of sentences.
+    """
+
+    def __init__(self):
+        """Initialize the SequenceMatcher Similarity metric"""
+        pass
+
+    def calculate(
+        self,
+        generated_text: str,
+        reference_text: str,
+        additional_params: Optional[Dict[str, Any]] = None,
+    ) -> float:
+        """
+        Calculate the SequenceMatcher Similarity ratio for a pair of generated and reference texts.
+
+        :param generated_text: The generated text to evaluate
+        :type generated_text: str
+        :param reference_text: The reference text to compare against
+        :type reference_text: str
+        :param additional_params: Additional parameters for SequenceMatcher, defaults to None
+        :type additional_params: Dict[str, Any], optional
+        :return: The SequenceMatcher Similarity ratio
+        :rtype: float
+        """
+        params = {}
+        if additional_params:
+            params.update(additional_params)
+
+        s_matcher = SequenceMatcher(None, generated_text, reference_text, **params)
+
+        return s_matcher.ratio()
+
+    def batch_calculate(
+        self,
+        generated_texts: Union[Iterable, np.ndarray, pd.Series],
+        reference_texts: Union[Iterable, np.ndarray, pd.Series],
+        additional_params: Optional[Dict[str, Any]] = None,
+    ) -> Union[np.ndarray, pd.Series, List[float]]:
+        """
+        Calculate SequenceMatcher Similarity for a batch of generated and reference texts.
+
+        :param generated_texts: Generated texts
+        :type generated_texts: Union[Iterable, np.ndarray, pd.Series]
+        :param reference_texts: Reference texts
+        :type reference_texts: Union[Iterable, np.ndarray, pd.Series]
+        :param additional_params: Additional parameters to pass to SequenceMatcher, defaults to None
+        :type additional_params: Dict[str, Any], optional
+        :return: A list, numpy array, or pandas Series of SequenceMatcher Similarity ratios
+        :rtype: Union[np.ndarray, pd.Series, List[float]]
+        """
+
+        gen_texts = to_iterable(generated_texts)
+        ref_texts = to_iterable(reference_texts)
+
+        params = {}
+        if additional_params:
+            params.update(additional_params)
+
+        if isinstance(gen_texts, np.ndarray) and isinstance(ref_texts, np.ndarray):
+            return np.array(
+                [self.calculate(gen, ref, params) for gen, ref in zip(gen_texts, ref_texts)]
+            )
+
+        elif isinstance(gen_texts, pd.Series) and isinstance(ref_texts, pd.Series):
+            return gen_texts.combine(ref_texts, lambda g, r: self.calculate(g, r, params))
+
+        else:
+            return [self.calculate(gen, ref, params) for gen, ref in zip(gen_texts, ref_texts)]
